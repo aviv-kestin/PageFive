d6345584cad9df1cbea5b24bba91c13fe47a6f73
aviv-kestin <143558875+aviv-kestin@users.noreply.github.com>
2024-02-27 17:10:39 -0500

    Add files via upload

---
+++ scripta-five.js
@@ -0,0 +1,21 @@
+$("document").ready(function() {
+
+
+    window.onscroll = function () {
+        scrollRotate();
+    };
+    
+    function scrollRotate() {
+        let image = document.getElementById("hex");
+        image.style.transform = "rotate(" + window.pageYOffset/4.45 + "deg)";
+    }
+
+
+
+
+
+
+
+
+
+});
